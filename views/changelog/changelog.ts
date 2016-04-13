746a4b439cc763a55e612fb16b60c4e9a37e8098
Aedaeum <exanix@gmail.com>
2016-04-13 15:40:42 -0700

    Overhauled changelog ref #4 All changelog data moved into changelog.json All changelog html is dynamically generated Changelog items are now fixed-height

--- views/changelog/changelog.ts
+++ views/changelog/changelog.ts
@@ -1,4 +1,55 @@
+import {inject} from 'aurelia-framework';
+import * as ajax from 'nanoajax';
 
-export class Changelog {
-  constructor() {}
+interface Section {
+  date: string;
+  timeStamp: string;
+  additions: string[];
+  changes: string[];
+  fixes: string[];
 }
+
+@inject(Element)
+export class Changelog {
+
+  sections: Section[];
+
+
+  constructor(body: HTMLElement) {
+
+    ajax.ajax({
+      url: '/changelog',
+      method: 'GET'
+    }, (code, res, req) => {
+      this.sections = JSON.parse(res);
+      console.log(this.sections);
+    });
+
+  }
+
+  attached() {
+    // let comps = Array.prototype.slice.call(
+    //               document.querySelectorAll('section.compartment')
+    //             ) as HTMLElement[]
+    //   , chgCount = 0;
+
+    // comps.forEach((v) => {
+    //   let changes = v.querySelectorAll('li').length;
+
+    //   chgCount += changes;
+    // });
+
+    // console.log(chgCount);
+
+
+  }
+}
+
+/*
+{"date": "asdf", "timeStamp": null,
+    "additions": [],
+    "changes": [],
+    "fixes": []
+  }
+
+*/
