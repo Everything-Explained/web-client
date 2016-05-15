148c399e3a5650e3b3433a7e085a8c92b3580678
Aedaeum <exanix@gmail.com>
2016-05-14 21:49:47 -0700

    Add about script (woops)

--- views/home/about.ts
+++ views/home/about.ts
@@ -1,6 +1,63 @@
 
 export class About {
+
+  private _email = this._toArray('exanix@gmail.com') as string[];
+  private _emailVisible = false;
+
   constructor() {
+
+  }
+
+
+
+  public test(ev: MouseEvent) {
+    if (this._emailVisible) return;
+    let el = (ev.target) as HTMLElement;
+    this._emailVisible = true;
+    el.innerText = '';
+    this._till(0, 0, el);
+
+
+  }
+
+  private _till(char: number, pos: number, el: HTMLElement) {
+
+    // The amount of characters to flip through per iteration
+    let charFlipAmount = 15;
+
+    let charArray = this._toArray(el.innerText) as string[];
+    if (charArray.length != char + 1) {
+      charArray.push('');
+    }
+
+    if (char < this._email.length) {
+      if (pos < charFlipAmount) {
+        charArray[char] = (String.fromCharCode((Math.random() * 42) + 48)).toUpperCase();
+        el.innerText = charArray.join('');
+        setTimeout(() => {
+          this._till(char, ++pos, el);
+        }, 10);
+      }
+      if (pos == charFlipAmount) {
+        console.log(pos);
+        charArray[char] = this._email[char];
+        el.innerText = charArray.join('');
+        this._till(++char, 0, el);
+      }
+
+    }
+
+    if (char == this._email.length && pos == charFlipAmount) {
+      el.innerHTML =  `<a href='mailto:${el.innerText}'>${el.innerText}</a>`;
+    }
+
+
+
+
+  }
+
+  private _toArray(val: Node|string) {
+    return Array.prototype.slice.call(val);
   }
 
 }
