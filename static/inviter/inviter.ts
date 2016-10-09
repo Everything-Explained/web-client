45d6428b4158b1c7da600c98a55db42a934658f4
Aedaeum <exanix@gmail.com>
2016-10-09 16:46:48 -0700

    Invite Generator completed Added server status checking Added simple validation Added Invite list from server Invites can no longer be created with limited uses and infinite time Increased maximum time to 365 days Re-ordered uses/days Fixed inputs not being parsed as ints

--- static/inviter/inviter.ts
+++ static/inviter/inviter.ts
@@ -3,28 +3,73 @@ declare var Materialize: any;
 
 
 let vmInviter = new Vue({
-  el: '.invite-form',
+  el: '#Container',
 
   data: {
-    inviteCode: null,
-    days: 1,
-    uses: 1
+    inviteCode: ' ',
+    days: '1',
+    uses: '1',
+    invites: []
   },
 
 
   attached: function() {
+    this.checkServerStatus();
   },
+
+
 
   computed: {
     hours: function() {
-      return (this.days === 0)
+      let days = parseInt(this.days);
+      return (days == 0)
         ? (365 * 10) * 24
-        : this.days * 24;
+        : days * 24;
     }
   },
 
+
+
+  filters: {
+    time: function(time: string) {
+      let t = parseFloat(time);
+
+      if (t == 0) {
+        return 'Expired';
+      }
+
+      if (t <= 24) {
+        return t.toFixed(0) + ' Hours';
+      }
+
+      if (t / 24 <= 30) {
+        return (t / 24).toFixed(0) + ' Days';
+      }
+
+      if (t / 24 / 30 <= 13) {
+        return 'Long';
+      }
+
+      return 'Unlimited';
+
+    },
+
+    toClass: function(time: string) {
+      if (~time.indexOf('Expired')) return 'expired';
+      if (~time.indexOf('Long')) return 'long';
+      if (~time.indexOf('Unlimited')) return 'unlimited';
+      return '';
+    }
+  },
+
+
+
   methods: {
     generate: function() {
+      this.$els.invitebox.classList.remove('invalid');
+      this.$els.inviteerror.classList.remove('active');
+
+
       Web.GET('/internal/invite', {
         fields: {
           hours: this.hours
@@ -35,15 +80,24 @@ let vmInviter = new Vue({
         }
         else {
           this.inviteCode = data.code;
-          // Textbox label does not animate on change
-          setTimeout(() => {
-            Materialize.updateTextFields();
-          }, 0);
+          this.updateTextFields();
         }
       });
     },
 
+
     save: function() {
+      let input = this.$els.invitebox as HTMLInputElement
+        , error = this.$els.inviteerror as HTMLElement
+        , invites = this.invites as any[];
+
+      if (!input.value.trim().length) {
+        input.classList.add('invalid');
+        error.innerText = 'Generate Invite';
+        error.classList.add('active');
+        return;
+      }
+
       Web.POST('/internal/addinvite', {
         fields: {
           code: this.inviteCode,
@@ -54,15 +108,122 @@ let vmInviter = new Vue({
           console.error(err.msg);
           return;
         }
-        console.info('Saved!');
+        this.inviteCode = ' ';
+        this.getInviteList();
       });
     },
 
+
+    // Textbox label does not animate on change
+    updateTextFields: function() {
+      setTimeout(() => {
+        Materialize.updateTextFields();
+      }, 0);
+    },
+
+
     rangeChanged: function(ev: KeyboardEvent) {
-      let obj = ev.target as HTMLInputElement;
+      let obj = ev.target as HTMLInputElement
+        , event = document.createEvent('HTMLEvents');
+
+
+      if (this.$els.invitebox.value.trim().length) {
+        this.$els.invitebox.value = ' ';
+      }
+
+      event.initEvent('change', true, false);
 
       if (obj.value == '0')
         obj.blur();
+
+      if (parseInt(obj.value) < 0) {
+        obj.value = '1';
+      }
+
+      if (obj.classList.contains('days')) {
+        if (obj.value == '0') {
+          this.$els.uses.value = 0;
+          this.$els.uses.dispatchEvent(event);
+          return;
+        }
+      }
+
+      if (obj.classList.contains('uses')) {
+        let days = this.$els.days as HTMLInputElement;
+        if (days.value == '0' && obj.value > '0') {
+          days.value = '365';
+          days.dispatchEvent(event);
+        }
+      }
+    },
+
+
+    getInviteList: function(delay?: number) {
+      setTimeout(() => {
+        Web.GET('/internal/listinvites', {}, (err, code, data: any[]) => {
+          if (err) {
+            console.log(err);
+            return;
+          }
+          data = data.sort((a, b) => {
+            return b.time_remaining - a.time_remaining;
+          });
+          this.invites = data;
+        });
+      }, delay || 0);
+
+    },
+
+
+    deleteInvite: function(code: string) {
+      Web.POST('/internal/deleteinvite', {
+        fields: {
+          code
+        }
+      }, (err, code, data) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+        this.getInviteList();
+      });
+    },
+
+
+    refreshInviteList: function(ev: MouseEvent) {
+      this.checkServerStatus(0, ev.target);
+    },
+
+
+    checkServerStatus: function(delay, loader: HTMLElement) {
+
+      if (loader) {
+        loader.classList.add('rotate');
+      }
+
+      let form = this.$els.form as HTMLElement
+        , status = this.$els.serverstatus as HTMLElement;
+
+      Web.GET('/internal/ping', {}, (err, code, data) => {
+        if (loader) {
+          loader.classList.remove('rotate');
+        }
+        if (err || code != 200) {
+          form.classList.add('hide');
+          status.classList.remove('hide');
+          status.classList.add('active');
+          return;
+        }
+        form.classList.remove('active');
+        form.classList.remove('hide');
+        form.classList.add('active');
+        status.classList.add('hide');
+        this.getInviteList(delay || 0);
+      });
+    },
+
+    refreshPage: function() {
+      this.checkServerStatus(0, this.$els.loader);
     }
   }
 });
