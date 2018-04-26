36db25e02116d40be4b14b7691e2cec51d0e6c0c
Aedaeum <exanix@gmail.com>
2018-04-25 20:06:34 -0700

    Added conditional blog route

--- au/src/components/home/home.ts
+++ au/src/components/home/home.ts
@@ -1,19 +1,28 @@
 import { RouterConfiguration, Router } from 'aurelia-router';
+import { inject } from 'aurelia-framework';
+import { Session } from '../../shared/models/session';
 
-
+@inject(Session)
 export class Home {
 
   public router: Router;
 
-  constructor() {}
+  constructor(public session: Session) {}
 
   configureRouter(config: RouterConfiguration, router: Router) {
-    config.map([
+
+    let routes = [
       {route: '', redirect: 'welcome'},
       {route: 'welcome', name: 'welcome', moduleId: 'components/home/pages/welcome', nav: true},
       {route: 'rules', name: 'rules', moduleId: 'components/home/pages/rules', nav: true},
       {route: ['faq', 'faq/:page'], name: 'faq', moduleId: 'components/home/pages/faq', nav: true }
-    ]);
+    ];
+
+    if (this.session.authed) {
+      routes.push({route: 'blog', name: 'blog', moduleId: 'components/home/pages/blog', nav: true});
+    }
+
+    config.map(routes);
     this.router = router;
   }
 
