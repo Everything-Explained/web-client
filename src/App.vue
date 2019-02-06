f7f0a7b09142001fba6a15a04613f2d70be7c8be
Jaeiya <exanix@gmail.com>
2019-02-06 08:45:40 -0800

    Add default structure & styles for index page
    ref #81

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,52 @@
 <template>
-  <div id="app">
+  <main id="PageTop">
+    <header class="global">
+      <div class="ribbon"></div>
+      <aside class="logo"></aside>
+    </header>
+
+    <!-- <aside id="VersionModal" class="modal-overlay">
+      <div class="modal-preload-container">
+        <div class="preloader"></div>
+      </div>
+      <section class="modal-container">
+        <header>
+          <span class="normal"></span>
+        </header>
+        <section class="code"></section>
+        <a href="javascript:void(0);" class="modal-close">&#10060;</a>
+      </section>
+    </aside> -->
+
+    <section id="TrayTop">
+      <aside class="watermark">
+        <span>{{ verType + version }}</span>
+      </aside>
+    </section>
+
+    <section id="PageMiddle">
+      <section id="TrayLeft">
+        <menu id="MainMenu">
+          <ul>
+            <li>
+              <span></span>
+            </li>
+          </ul>
+        </menu>
+      </section>
+
+      <router-view id="PageContent" />
+    </section>
+
+  </main>
+  <!-- <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
-  </div>
+  </div> -->
 </template>
 
-<style lang="scss">
-
-// #app
-//   font-family: 'Avenir', Helvetica, Arial, sans-serif;
-//   -webkit-font-smoothing: antialiased;
-//   -moz-osx-font-smoothing: grayscale;
-//   text-align: center;
-//   color: #2c3e50;
-// #nav {
-//   padding: 30px;
-//   a {
-//     font-weight: bold;
-//     color: #2c3e50;
-//     &.router-link-exact-active {
-//       color: #42b983;
-//     }
-//   }
-// }
-</style>
+<script type='ts' src='./app.ts'></script>
+<style lang="sass" src="./app.sass"></style>
