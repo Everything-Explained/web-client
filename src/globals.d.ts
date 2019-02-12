d9f12c9dcaffb97bbecec82f85a7fe185f76c783
Jaeiya <exanix@gmail.com>
2019-02-11 16:49:01 -0800

    Add $dataImages typings

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import { MiniModal } from './libs/minimodal';
+import imageData from '@/assets/data-images.json';
 
 declare module "vue/types/vue" {
   interface Vue {
     readonly $modal: MiniModal;
+    readonly $dataImages: typeof imageData;
   }
 }
