3843f3a4cf0c7812045f9adc5f182e330fbb2d76
Jaeiya <exanix@gmail.com>
2019-04-22 21:50:30 -0700

    Add support for static file copy to vue config

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,24 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   css: {
     sourceMap: true,
     extract: false
   },
+
+  configureWebpack: {
+    plugins: [
+      new CopyPlugin([
+        {
+          from: path.join(__dirname, 'src/views/static/*.*'),
+          to: path.join(__dirname, 'dist/static'),
+          flatten: true
+        }
+      ])
+    ],
+  },
+
 
   chainWebpack: config => {
     const apiClientLocation = process.env.API_TYPE // mock or server
