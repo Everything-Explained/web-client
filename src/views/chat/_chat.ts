617ddfc4415015132e2858dcbfac0515abc8c538
Jaeiya <exanix@gmail.com>
2019-05-01 17:33:38 -0700

    Update function arg order and add client event

--- src/views/chat/_chat.ts
+++ src/views/chat/_chat.ts
@@ -4,9 +4,8 @@ import Display from './components/display/Display.vue';
 import Userlist from './components/userlist/Userlist.vue';
 import Commander from './components/cmdinput/Commander.vue';
 import Utils from '@/libs/utils';
-import { IMessage, MessageType, MessageScale } from './components/display/_display';
 import ChatSocket, { ClientEvent } from './_socket';
-import { MsgPriority } from './components/message/_message';
+import { MsgPriority, MsgScale, IMessage, MsgType } from './components/message/_message';
 
 @Component({
   components: {
@@ -17,16 +16,17 @@ import { MsgPriority } from './components/message/_message';
 })
 export default class Chat extends Vue {
 
-  public displayScale: MessageScale = 'normal';
+  public displayScale: MsgScale = 'normal';
 
 
-  private sio!: SocketIOClient.Socket;
+  private readonly sio!: SocketIOClient.Socket;
   private readonly sock = new ChatSocket(
     'https://localhost:3003',
     'BKL8YW2OZUNFLC6RJLS7YN7T' || this.$api.rid
   );
 
   private messages: IMessage[] = [];
+
 
 
   get thisView() {
@@ -39,26 +39,26 @@ export default class Chat extends Vue {
 
 
   created() {
-    sock.on(ClientEvent.SRVMSG, (content, priority) => {
+    this.sock.on(ClientEvent.SERVERMSG, (content, priority) => {
       this.addMessage(
         'Server',
         content,
-        priority,
-        'server'
+        'server',
+        priority
       )
     })
-    // this.sio = io.connect('https://localhost:3003', { forceNew: true });
-    // this.sio.on('connect', () => {
-    //   console.log('connected successfully');
-    //   this.sio.on('room-setup', (name: string, d: string, clients: []) => {
-    //     console.log('joined room', name, d, clients);
-    //   })
-    //   this.sio.emit('hand-shake', this.$api.rid);
-    // })
+    .on(ClientEvent.CLIENTMSG, (content, priority) => {
+      this.addMessage(
+        'Client',
+        content,
+        'server',
+        priority
+      )
+    })
   }
 
 
-  addMessage(alias: string, content: string, priority?: MsgPriority, type?: MessageType) {
+  addMessage(alias: string, content: string, type?: MsgType, priority?: MsgPriority) {
     const avatar = 'https://lh4.googleusercontent.com/-jm9RnjaBMrI/AAAAAAAAAAI/AAAAAAAAAfM/_RhlOKf4IlU/photo.jpg?sz=96'
     this.messages.push(
       {
