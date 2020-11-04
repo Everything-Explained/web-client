e6c53c8b849bffb491c11064ed8274b56d250c63
Jaeiya <exanix@gmail.com>
2020-11-04 15:27:06 -0800

    Move and copy relevant files/code from old project

--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,45 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+
+
+
+const getElement = (id: string) => {
+  return document.getElementById(id) as HTMLElement;
+}
+
+
+const browserIsSupported = () => {
+  const isValidBrowser =
+       navigator.cookieEnabled != undefined
+    || window['Promise']
+    || window['DOMParser']
+    || document.body.dataset
+    || (new DOMParser()).parseFromString
+    || document.importNode
+  ;
+  if (!isValidBrowser) {
+    const el = getElement('Failure');
+    el.style.display = 'inline-block';
+    return false;
+  }
+  return true;
+}
+
+
+const cookiesAreEnabled = () => {
+  if (!navigator.cookieEnabled) {
+    const el = getElement('NoCookies');
+    el.style.display = 'inline-block';
+    return false;
+  }
+  return true;
+}
+
+
+if (cookiesAreEnabled() && browserIsSupported()) {
+  createApp(App)
+    .use(router)
+    .mount('#app')
+  ;
+}
