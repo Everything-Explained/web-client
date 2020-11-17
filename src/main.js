e0609084250d0f0553e6b776644498680f67b3d4
Jaeiya <exanix@gmail.com>
2020-11-16 16:28:11 -0800

    Fix: `main` missing browser compatibility logic

--- src/main.js
+++ src/main.js
@@ -1,9 +1,45 @@
+/* eslint-disable */
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 import './main.css';
 
-createApp(App)
-  .use(router)
-  .mount('#app')
-;
+
+const getElement = (id) => {
+  return document.getElementById(id);
+};
+
+
+const browserIsSupported = (() => {
+  const isValidBrowser =
+       navigator.cookieEnabled != undefined
+    || window['Promise']
+    || window['DOMParser']
+    || document.body.dataset
+    || (new DOMParser()).parseFromString
+    || document.importNode
+  ;
+  if (!isValidBrowser) {
+    const el = getElement('Failure');
+    el.style.display = 'inline-block';
+    return false;
+  } return true;
+})();
+
+
+const cookiesAreEnabled = (() => {
+  if (!navigator.cookieEnabled) {
+    const el = getElement('NoCookies');
+    el.style.display = 'inline-block';
+    return false;
+  } return true;
+})();
+
+
+
+if (cookiesAreEnabled && browserIsSupported) {
+  createApp(App)
+    .use(router)
+    .mount('#app')
+  ;
+}
