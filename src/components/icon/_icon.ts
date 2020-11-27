322c22d79c67b10c802fc9fb9e3db0e4844e8f3a
Jaeiya <exanix@gmail.com>
2020-11-26 16:55:21 -0800

    Add new dripicon names to icon component map

--- src/components/icon/_icon.ts
+++ src/components/icon/_icon.ts
@@ -4,8 +4,23 @@ import { defineComponent, onMounted, ref } from "vue";
 type StringMap = { [key: string]: string };
 
 const iconMap: StringMap = {
-  'calendar': 'M',
-  'clock': 'Y',
+  'clock'      : 'Y',
+  'calendar'   : 'M',
+  'cross'      : '9',
+  'gear'       : '~',
+  'right-chev' : 'V',
+  'left-chev'  : 'U',
+  'log-in'     : '<',
+  'log-out'    : '=',
+  'heart'      : '&#xe004',
+  'home'       : '&#xe006',
+  'mail'       : '&#xe015',
+  'menu'       : '&#xe020',
+  'star'       : '&#xe045',
+  'thumb-up'   : '&#xe04e',
+  'thumb-down' : '&#xe04d',
+  'trash'      : '&#xe053',
+  'user'       : '&#xe056',
 };
 
 
