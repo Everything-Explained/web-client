d8c64e9997f5a38c2178684bf43bc15a2a89a3b3
Jaeiya <exanix@gmail.com>
2020-12-04 15:00:05 -0800

    CSS(menu): use proper BEM format and add all color variables

--- src/components/menu/menu.vue
+++ src/components/menu/menu.vue
@@ -1,16 +1,16 @@
 <template>
-  <menu ref='menu' class="main__menu" :class="{ '--opened': opened }">
-    <header class="menu__header">
-      <div class="menu-header_ribbon"></div>
-      <span class="menu-header_text">MENU</span>
+  <menu ref='menu' class="app-menu" :class="{ '--opened': opened }">
+    <header class="app-menu__header">
+      <div class="app-menu__header_ribbon"></div>
+      <span class="app-menu__header_text">MENU</span>
       <icon
-        class='menu-header_exit-icon'
+        class='app-menu__header_exit-icon'
         :type='"cross"'
         @mousedown="closeMenu"
       ></icon>
     </header>
     <ul>
-      <li class="menu_item" v-for="(route, i) of routes" :key="i" @click="closeMenu">
+      <li class="app-menu_item" v-for="(route, i) of routes" :key="i" @click="closeMenu">
         <router-link :to="route.path">{{ route.name }}</router-link>
       </li>
     </ul>
