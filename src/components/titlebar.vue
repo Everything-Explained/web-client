4c73ad5e8adb93f1f1de39ff29019537d815889f
Jaeiya <exanix@gmail.com>
2020-12-05 15:35:21 -0800

    Add transition and props to titlebar component

--- src/components/titlebar.vue
+++ src/components/titlebar.vue
@@ -5,30 +5,40 @@
       @mousedown="openMenu"
       :class="{ '--menu-open': isMenuOpen }" :type='"menu"'
     ></icon>
-    <div class="title-bar__text" :key="title">{{title}}</div>
+    <transition
+      name="fade" :duration="{ enter, leave }" mode="out-in"
+    >
+      <div class="title-bar__text" :key="title">{{title}}</div>
+    </transition>
   </div>
 </template>
 
 
 
 <script lang='ts'>
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, reactive, toRefs } from "vue";
 import { useStore } from "vuex";
 import { VuexStore } from "../vuex/vuex-store";
 import icon from './icon.vue';
 
-export default defineComponent({
-  components: {
-    icon,
-  },
-  setup() {
-    const store = useStore<VuexStore>();
 
-    const openMenu   = () => store.commit('open-menu');
+export default defineComponent({
+  components : { icon, },
+  props      : { easeIn: Number, easeOut: Number, },
+
+  setup(props) {
+    const store      = useStore<VuexStore>();
     const isMenuOpen = computed(() => store.state.isMenuOpen);
     const title      = computed(() => store.state.pageTitle);
+    const duration = {
+      enter: props.easeIn ?? 400,
+      leave: props.easeOut ?? 400
+    };
+    const openMenu = () => store.commit('open-menu');
 
-    return { openMenu, isMenuOpen, title };
+    return { openMenu, isMenuOpen, title, ...duration };
   }
 })
+
+
 </script>
