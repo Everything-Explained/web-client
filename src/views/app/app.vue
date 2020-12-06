791ba09841f26d38993d0973981a5c7b67327a9a
Jaeiya <exanix@gmail.com>
2020-12-06 13:25:01 -0800

    Integrate the main menu under the header

--- src/views/app/app.vue
+++ src/views/app/app.vue
@@ -1,12 +1,12 @@
 <template>
-  <main-menu></main-menu>
   <div id="App" class="app-container">
     <div class="app-ribbon"></div>
-    <header class="app-header">
+    <header id='AppHeader' class="app-header">
       <div class="app-header__title">Everything Explained</div>
     </header>
     <section class="app-body">
-      <div class="app-body__content">
+      <main-menu :contentId='"AppBodyContent"'></main-menu>
+      <div id='AppBodyContent' class="app-body__content">
         <router-view v-slot='{ Component }'>
           <transition name='fade' mode="out-in">
             <component :is="Component" />
