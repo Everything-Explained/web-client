8e2478d972f9f0323f6f755383f6d6d53e99a2e5
Jaeiya <exanix@gmail.com>
2020-12-10 21:57:58 -0800

    Fix(red33m): commented out tests, color highlight, test border

--- src/views/red33m/red33m.vue
+++ src/views/red33m/red33m.vue
@@ -7,13 +7,12 @@
         :key="i"
         class="red33m-video"
         :data-title='v[1]'
+        @click="openVideo('//www.youtube.com/watch?v='+ v[0] +'&rel=0')"
       >
-      <!--         @click="openVideo('//www.youtube.com/watch?v='+ v[0] +'&rel=0')" -->
         <lazy-img
           :src="'//img.youtube.com/vi/' + v[0] + '/0.jpg'"
           class="red33m-video_thumbnail-container"
         />
-        <!-- <span class="red33m-video_title"></span> -->
       </div>
     </div>
   </div>
