681cbedb20973f401312deec67762fb7115ef5fb
Jaeiya <exanix@gmail.com>
2020-12-11 16:53:02 -0800

    Add on/off switch component
    This will be used later when we have the user database set up
    with settings that can be changed.

---
+++ src/components/onoff-switch.vue
@@ -0,0 +1,10 @@
+<template>
+  <div class="onoff-switch input-switch" @click="toggle()">
+    <input type="checkbox" :checked="state" />
+    <span class="switch-text-off">OFF</span>
+    <span class="slider"></span>
+    <span class="switch-text-on">ON</span>
+  </div>
+</template>
+
+<script lang="ts" src='./onoff-switch.ts'></script>
