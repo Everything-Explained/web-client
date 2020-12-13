dee8ce5a0ce4ed078295c6ac6778682d11bf7d6c
Jaeiya <exanix@gmail.com>
2020-12-12 16:50:37 -0800

    Add toggle component
    This will be extended in the future to support multiple
    items; not just two.

---
+++ src/components/toggle.vue
@@ -0,0 +1,13 @@
+<template>
+  <fieldset>
+    <legend>{{ legend }}</legend>
+    <div class="toggle">
+      <input type="radio" name="sizeBy" value="weight" id="sizeWeight" checked="checked" />
+      <label @click="toggle(false)" for="sizeWeight">{{ leftText }}</label>
+      <input type="radio" name="sizeBy" value="dimensions" id="sizeDimensions" />
+      <label @click="toggle(true)" for="sizeDimensions">{{ rightText }}</label>
+    </div>
+  </fieldset>
+</template>
+
+<script lang="ts" src='./toggle.ts'></script>
