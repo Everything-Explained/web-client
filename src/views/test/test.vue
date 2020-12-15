f417e06112d8ed1783d149b773b518a856c26af8
Jaeiya <exanix@gmail.com>
2020-12-14 16:57:07 -0800

    Add test page and remaining WIP styles

---
+++ src/views/test/test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="test">
+    <title-bar></title-bar>
+    <br>
+    <div class="container-yes" style="width: 300px; text-align: center; margin: 0 auto;">
+      <toggle :callback='toggleSort' :legend='"Video Sort"'></toggle>
+    </div>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+  </div>
+</template>
+
+<script lang="ts" src='./_test.ts'></script>
