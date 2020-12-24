b8d1e67edcb058c3997c1d5ab7b87c4d0e3b0046
Jaeiya <exanix@gmail.com>
2020-12-24 11:28:20 -0800

    Add `release` and `watch-css` gulp tasks

--- build/gulpfile.ts
+++ build/gulpfile.ts
@@ -1,4 +1,23 @@
 import gulp from 'gulp';
 import { bundleMDPages } from './scripts/build_md';
+import { cleanClient, compileClient, compressToBrotli, compressToGzip, copyClient } from './scripts/build_client';
+import { buildCSS, watchCSS } from './scripts/build_css';
+import config from './config.json';
+
+
 const task     = gulp.task;
+const parallel = gulp.parallel;
+const series   = gulp.series;
+
+task('release',
+  series(
+    parallel(bundleMDPages, buildCSS),
+    compileClient,
+    parallel(compressToBrotli, compressToGzip, cleanClient),
+    copyClient
+  )
+);
+
+task('compress', compressToBrotli);
 task('build-md', bundleMDPages);
+task('watch-css', watchCSS);
