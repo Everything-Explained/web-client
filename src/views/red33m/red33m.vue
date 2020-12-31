1f2147ab7aeda4bdd53f23004f0002229bcfa0ae
Jaeiya <exanix@gmail.com>
2020-12-30 18:38:00 -0800

    upd(red33m): use page data cache for videos with preloader

--- src/views/red33m/red33m.vue
+++ src/views/red33m/red33m.vue
@@ -1,28 +1,34 @@
 <template>
   <div class="red33m">
     <title-bar></title-bar>
-    <toggle
-      :legend='"Sort By"'
-      :leftText='"Oldest"'
-      :rightText='"Latest"'
-      :callback='toggle'
-      :prevent='isLoading'
-      class='red33m-toggle'
-    ></toggle>
-    <div class="red33m-video-list">
-      <div
-        v-for="(v, i) of videos"
-        :key="i"
-        class="red33m-video"
-        :data-title='v[1]'
-        @click="openVideo('//www.youtube-nocookie.com/embed/'+ v[0] +'?rel=0')"
-      >
-        <lazy-img
-          :src="'//img.youtube.com/vi/' + v[0] + '/0.jpg'"
-          class="red33m-video_thumbnail-container"
-        />
+    <transition name='fade' mode="out-in">
+      <preloader class="data-loader" v-if="getVideos.isRunning"></preloader>
+      <div v-else>
+        <toggle
+          :legend='"Sort By"'
+          :leftText='"Oldest"'
+          :rightText='"Latest"'
+          :callback='toggle'
+          :prevent='isLoading'
+          class='red33m-toggle'
+        ></toggle>
+        <div class="red33m-video-list">
+          <div
+            v-for="(v, i) of videos"
+            :key="i"
+            class="red33m-video"
+            :data-title='v.title'
+            @click="openVideo('//www.youtube-nocookie.com/embed/'+ v.id +'?rel=0')"
+          >
+            <lazy-img
+              :src="'//img.youtube.com/vi/' + v.id + '/0.jpg'"
+              class="red33m-video_thumbnail-container"
+            />
+          </div>
+        </div>
       </div>
-    </div>
+
+    </transition>
   </div>
 </template>
 
