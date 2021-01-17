4b1e78bae1ba07951aec60d07e9a12a01119f5c8
Jaeiya <exanix@gmail.com>
2021-01-16 17:40:32 -0800

    upd(ee-video): Add description and widget support
    This also fixes a known issue when scaling thumbnails with
    black bars.
    
    ref #31

--- src/components/ee-video.ts
+++ src/components/ee-video.ts
@@ -1,20 +1,24 @@
-import { computed, defineComponent } from "vue";
+import { computed, defineComponent, ref } from "vue";
 import Lazyimg from '@/components/lazyimg.vue';
+import Icon from '@/components/icon.vue';
 
 export default defineComponent({
   components: {
     'lazy-img': Lazyimg,
+    'icon': Icon,
   },
   props: {
-    title: String,
+    desc: String,
     videoId: String,
   },
   setup(props) {
-    const titleRef     = computed(() => props.title);
     const videoIDRef   = computed(() => props.videoId);
+    const descRef      = computed(() => props.desc);
+    const descStateRef = ref(false);
     const thumbnailRef = computed(() =>
       `//img.youtube.com/vi/${videoIDRef.value}/0.jpg`
     );
+
 
     const openVideo = () => {
       window.open(
@@ -23,10 +27,22 @@ export default defineComponent({
       );
     };
 
+    /** Prevents mouse events from propagating to parent elements */
+    const preventBubble = (e: MouseEvent) => {
+      e.preventDefault();
+      e.stopImmediatePropagation();
+    };
+
+    const setDescState = (e: MouseEvent, state: 'closed'|'open') => {
+      preventBubble(e);
+      descStateRef.value = state == 'open';
+    };
+
     return {
-      title    : titleRef,
       thumbnail: thumbnailRef,
-      openVideo
+      description: descRef,
+      descState: descStateRef,
+      openVideo, preventBubble, setDescState
     };
   }
 });
--- src/components/ee-video.vue
+++ src/components/ee-video.vue
@@ -1,10 +1,27 @@
 <template>
-  <div
-    class="ee-video"
-    :data-title='title'
-    @click="openVideo"
-  >
-    <lazy-img :src='thumbnail'></lazy-img>
+  <div class="ee-video">
+    <div class="ee-video_img-container" @click="openVideo">
+      <lazy-img :src='thumbnail' class="ee-video_img"></lazy-img>
+      <icon class="ee-video-desc_open-widget"
+        :type='"info"'
+        @click="(e) => setDescState(e, 'open')"
+        :class="{ '--open': descState }"
+      ></icon>
+      <div class="ee-video_desc"
+        @click="preventBubble"
+        :class="{ '--open': descState }"
+      >
+        <icon class="ee-video-desc_close-widget"
+          :type='"cross"'
+          @click="(e) => setDescState(e, 'closed')"
+        ></icon>
+        <div class="ee-video_content-container">
+          <h1>Description</h1>
+          <span class="--default-scrollbar" v-html="description"></span>
+        </div>
+      </div>
+    </div>
+    <div class="ee-video_title"><slot></slot></div>
   </div>
 </template>
 
