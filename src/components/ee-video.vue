773077aab29a29f87e2950b6fabc546b15892b9f
Jaeiya <exanix@gmail.com>
2021-01-17 16:17:12 -0800

    clean(ee-video): fix spacing and missing classes
    This also changes the style of description links

--- src/components/ee-video.vue
+++ src/components/ee-video.vue
@@ -3,21 +3,22 @@
     <div class="ee-video_img-container">
       <lazy-img :src='thumbnail' class="ee-video_img"></lazy-img>
       <icon class="ee-video_widget ee-video_widget-open-video"
-        :type='"export"'
-        :class="{ '--open': descState }"
         @click="openVideo"
+        :class="{ '--open': descState }"
+        :type='"export"'
       ></icon>
       <icon class="ee-video_widget ee-video_widget-open-desc"
-        :type='"info"'
+        v-if="description"
         @click="setDescState('open')"
         :class="{ '--open': descState }"
+        :type='"info"'
       ></icon>
       <div class="ee-video_desc"
         :class="{ '--open': descState }"
       >
         <icon class="ee-video_widget ee-video_widget-close-desc"
-          :type='"cross"'
           @click="setDescState('closed')"
+          :type='"cross"'
         ></icon>
         <div class="ee-video-desc_text">
           <h1>Description</h1>
