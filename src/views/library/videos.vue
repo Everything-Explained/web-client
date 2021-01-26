6893205d9a1cecc0c6f40cff63089654a9a7daeb
Jaeiya <exanix@gmail.com>
2021-01-26 13:09:03 -0800

    upd(library): Add preloader to videos

--- src/views/library/videos.vue
+++ src/views/library/videos.vue
@@ -1,20 +1,25 @@
 <template>
   <div class="lib-videos">
     <title-bar :easeIn='350' :easeOut='350'></title-bar>
-    <div class="category"
-      v-for="(cat, i) of categories"
-      :key="i"
-    >
-      <div class="category_name">{{ cat.name }}</div>
-      <div class="cat_video-tray --default-scrollbar">
-        <ee-video v-for="(v, i) of cat.videos" :key="i"
-          :videoId='v.id'
-          :desc='v.content'
-          class="lib-videos_video"
-        >{{ v.title }}</ee-video>
+    <transition name='fade' mode='out-in'>
+      <div class="preloader page" v-if="!categories"></div>
+      <div v-else>
+        <div class="category"
+          v-for="(cat, i) of categories"
+          :key="i"
+        >
+          <div class="category_name">{{ cat.name }}</div>
+          <div class="cat_video-tray --default-scrollbar">
+            <ee-video v-for="(v, i) of cat.videos" :key="i"
+              :videoId='v.id'
+              :desc='v.content'
+              class="lib-videos_video"
+            >{{ v.title }}</ee-video>
+          </div>
+        </div>
+        <ee-footer></ee-footer>
       </div>
-    </div>
-    <ee-footer></ee-footer>
+    </transition>
   </div>
 </template>
 
