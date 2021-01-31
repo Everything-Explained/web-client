6417f87aa6f242b2d97705d2f2613f95196553c8
Jaeiya <exanix@gmail.com>
2021-01-31 14:11:51 -0800

    upd(titlebar): separate model into separate file
    This allows us to auto-complete imports in vscode though
    you still have to manually add the .vue ext

--- src/components/layout/titlebar.vue
+++ src/components/layout/titlebar.vue
@@ -1,7 +1,6 @@
 <template>
   <div class="title-bar">
-    <icon
-      class="title-bar__menu-icon"
+    <icon class="title-bar__menu-icon"
       @mousedown="openMenu"
       :class="{ '--menu-open': isMenuOpen }" :type='"menu"'
     ></icon>
@@ -15,30 +14,4 @@
 
 
 
-<script lang='ts'>
-import { computed, defineComponent, reactive, toRefs } from "vue";
-import { useStore } from "vuex";
-import { VuexStore } from "@/vuex/vuex-store";
-import icon from '@/components/ui/icon.vue';
-
-
-export default defineComponent({
-  components : { icon, },
-  props      : { easeIn: Number, easeOut: Number, },
-
-  setup(props) {
-    const store      = useStore<VuexStore>();
-    const isMenuOpen = computed(() => store.state.isMenuOpening);
-    const title      = computed(() => store.state.pageTitle);
-    const duration = {
-      enter: props.easeIn ?? 400,
-      leave: props.easeOut ?? 400
-    };
-    const openMenu = () => store.commit('open-menu');
-
-    return { openMenu, isMenuOpen, title, ...duration };
-  }
-})
-
-
-</script>
+<script lang='ts' src='./titlebar'></script>
