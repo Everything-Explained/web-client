167255d64d44f35a07f9d2f51bfaebe6d6c3063e
Jaeiya <exanix@gmail.com>
2021-02-03 14:55:31 -0800

    ee-input-field: combine model and view

--- src/components/ui/ee-input-field.vue
+++ src/components/ui/ee-input-field.vue
@@ -14,4 +14,43 @@
 </template>
 
 
-<script lang='ts' src='./ee-input-field'></script>
+<script lang='ts'>
+import { defineComponent } from "vue";
+
+
+const _inputTypes = ['text', 'area', 'email', 'password'];
+
+
+export default defineComponent({
+  props: {
+    'modelValue': String,
+    name: {
+      type: String,
+      default: null,
+    },
+    type: {
+      type: String,
+      default: 'text',
+      validator: (val: string) => _inputTypes.includes(val),
+    },
+    maxlength: {
+      type: Number,
+      default: 255,
+    }
+  },
+  emits: ['update:modelValue'],
+  setup(props) {
+    const base36RndNum = Math.floor(Math.random() * 10000).toString(36);
+    const base36Time = Date.now().toString(36);
+    const id = `i${base36Time}${base36RndNum}`;
+    if (props.maxlength > 255) throw Error('ee-input-field:: maxLength should be less than 255.');
+
+    return {
+      id,
+      type: props.type,
+      maxLength: props.maxlength,
+      getVal: (e: Event) => (e.target as HTMLInputElement).value
+    };
+  }
+});
+</script>
