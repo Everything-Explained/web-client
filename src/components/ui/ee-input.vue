26b0c7db0358ae2e56ca64861db5e226230d2985
Jaeiya <exanix@gmail.com>
2021-02-09 16:49:56 -0800

    upd(ee-input): add error message with invalid types
    The built-in validator is a warning with littler information
    about where it came from.

--- src/components/ui/ee-input.vue
+++ src/components/ui/ee-input.vue
@@ -32,15 +32,8 @@ const _inputTypes = ['text', 'area', 'email', 'password'];
 export default defineComponent({
   props: {
     'modelValue': String,
-    name: {
-      type: String,
-      default: null,
-    },
-    type: {
-      type: String,
-      default: 'text',
-      validator: (val: string) => _inputTypes.includes(val),
-    },
+    name: { type: String, default: null, },
+    type: { type: String, default: 'text' },
     maxlength: {
       type: Number,
       default: 255,
@@ -52,7 +45,8 @@ export default defineComponent({
     const base36RndNum = Math.floor(Math.random() * 10000).toString(36);
     const base36Time = Date.now().toString(36);
     const id = `i${base36Time}${base36RndNum}`;
-    if (props.maxlength > 255) throw Error('ee-input-field:: maxLength should be less than 255.');
+    if (props.maxlength > 255) throw Error('ee-input:: maxLength should be less than 255.');
+    if (!_inputTypes.includes(props.type)) throw Error('ee-input:: invalid input type')
 
     function autoHeight(e: Event) {
       const el = e.target as HTMLTextAreaElement;
