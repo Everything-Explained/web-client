1d7c54f3c0021837de1bda7cd06e9d1e7bd10832
Jaeiya <exanix@gmail.com>
2021-02-19 08:35:06 -0800

    clean(ee-button): move logic into Vue

--- src/components/ui/ee-button.vue
+++ src/components/ui/ee-button.vue
@@ -9,4 +9,31 @@
 
 
 
-<script src='./ee-button' lang='ts'></script>
+<script lang='ts'>
+import { computed, defineComponent } from "vue";
+
+
+const _buttonThemes = ['neutral', 'attention', 'dangerous'];
+
+
+export default defineComponent({
+  props: {
+    theme:    { type: String,  default: 'neutral' },
+    loading:  { type: Boolean, default: false,     },
+    disabled: { type: Boolean, default: false,     },
+  },
+  setup(props) {
+    if (!_buttonThemes.includes(props.theme))
+      throw Error(`ee-button:: invalid button type ${props.theme}`)
+    ;
+    const isLoading  = computed(() => props.loading);
+    const isDisabled = computed(() => {
+      // Prevent clicks when loading
+      if (isLoading.value) return true;
+      return props.disabled;
+    });
+
+    return { isDisabled, isLoading };
+   }
+});
+</script>
