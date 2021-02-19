9a3ccc806849f60797ab34d6c942f80d649677d9
Jaeiya <exanix@gmail.com>
2021-02-19 08:49:18 -0800

    clean(ee-video): align with linter and move logic to Vue

--- src/components/ui/ee-video.vue
+++ src/components/ui/ee-video.vue
@@ -1,36 +1,71 @@
 <template>
   <div class="ee-video">
     <div class="ee-video_img-container">
-      <ee-img :src='thumbnail' class="ee-video_img"></ee-img>
-      <ee-icon class="ee-video_widget ee-video_widget-open-video"
-        @click="openVideo"
-        :class="{ '--open': descState }"
-        :type='"export"'
-      ></ee-icon>
-      <ee-icon class="ee-video_widget ee-video_widget-open-desc"
-        v-if="description"
-        @click="setDescState('open')"
-        :class="{ '--open': descState }"
-        :type='"info"'
-      ></ee-icon>
-      <div class="ee-video_desc"
-        :class="{ '--open': descState }"
-      >
+      <ee-img :src="thumbnail" class="ee-video_img" />
+      <ee-icon :class="['ee-video_widget ee-video_widget-open-video', { '--open': descState }]"
+               type="export"
+               @click="openVideo"
+      />
+      <ee-icon v-if="description"
+               :class="['ee-video_widget ee-video_widget-open-desc', { '--open': descState }]"
+               type="info"
+               @click="setDescState('open')"
+      />
+      <div :class="['ee-video_desc', { '--open': descState }]">
         <ee-icon class="ee-video_widget ee-video_widget-close-desc"
-          @click="setDescState('closed')"
-          :type='"cross"'
-        ></ee-icon>
+                 type="cross"
+                 @click="setDescState('closed')"
+        />
         <div class="ee-video-desc_text">
           <h1>Description</h1>
-          <span class="--default-scrollbar"
-            v-html="description"
-          ></span>
+          <span class="--default-scrollbar" v-html="description" />
         </div>
       </div>
     </div>
-    <div class="ee-video_title"><slot></slot></div>
+    <div class="ee-video_title">
+      <slot />
+    </div>
   </div>
 </template>
 
 
-<script lang='ts' src='./ee-video'></script>
+<script lang='ts'>import { computed, defineComponent, ref, toRefs } from "vue";
+import eeIconVue from '@/components/ui/ee-icon.vue';
+import eeImgVue from "./ee-img.vue";
+
+export default defineComponent({
+  components: {
+    'ee-img': eeImgVue,
+    'ee-icon': eeIconVue,
+  },
+  props: {
+    desc    : { type: String, default: '' },
+    videoId : { type: String, default: '' },
+  },
+  setup(props) {
+    const { videoId, desc } = toRefs(props);
+    const descStateRef = ref(false);
+    const thumbnailRef = computed(() =>
+      `//img.youtube.com/vi/${videoId.value}/0.jpg`
+    );
+
+    function openVideo() {
+      window.open(
+        `//www.youtube-nocookie.com/embed/${videoId.value}?rel=0`,
+        '_blank'
+      );
+    }
+
+    function setDescState(state: 'closed'|'open') {
+      descStateRef.value = state == 'open';
+    }
+
+    return {
+      thumbnail: thumbnailRef,
+      description: desc,
+      descState: descStateRef,
+      openVideo, setDescState
+    };
+  }
+});
+</script>
