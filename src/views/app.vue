0abd053e3a14177672fb429eb5a409f77bad4bdc
Jaeiya <exanix@gmail.com>
2021-02-19 07:11:52 -0800

    clean(app): align with linter and move logic into view

--- src/views/app.vue
+++ src/views/app.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="App" class="app-container">
-    <div class="app-ribbon"></div>
-    <header id='AppHeader' class="app-header">
-      <div class="app-header__title">Everything Explained</div>
+    <div class="app-ribbon" />
+    <header id="AppHeader" class="app-header">
+      <div class="app-header__title">
+        Everything Explained
+      </div>
     </header>
     <section class="app-body">
-      <main-menu
-        :headerId='"AppHeader"'
-        :contentId='"AppBodyContent"'
-      ></main-menu>
-      <div id='AppBodyContent' class="app-body__content">
-        <router-view v-slot='{ Component }'>
-          <transition name='fade' mode="out-in">
+      <main-menu :header-id="'AppHeader'" :content-id="'AppBodyContent'" />
+      <div id="AppBodyContent" class="app-body__content">
+        <router-view v-slot="{ Component }">
+          <transition name="fade" mode="out-in">
             <component :is="Component" />
           </transition>
         </router-view>
@@ -20,4 +19,55 @@
   </div>
 </template>
 
-<script lang='ts' src='./app'></script>
+
+
+<script lang='ts'>
+import { defineComponent, onMounted, ref, watch } from "vue";
+import { useRoute, useRouter } from "vue-router";
+import eeMenuVue from "@/components/layout/ee-menu.vue";
+
+export default defineComponent({
+  components: {
+    'main-menu': eeMenuVue,
+  },
+  setup() {
+    // const version     = '36';
+    // const versionType = 'α';
+    const body          = ref<HTMLBodyElement|null>(null);
+    const blogScrollPos = ref(0);
+    const router        = useRouter();
+    const route         = useRoute();
+
+    onMounted(() => {
+      body.value = document.getElementsByTagName('body')[0];
+    });
+
+    const setScrollTop = (top: number) => {
+      // Prevents user from noticing scroll reset.
+      // Resets when view is hidden in transition.
+      setTimeout(() => body.value!.scrollTop = top, 450);
+    };
+
+    const setBlogScrollPos = () => {
+      if (route.path.includes('/blog/')) {
+        blogScrollPos.value = body.value!.scrollTop;
+        setScrollTop(0);
+      }
+      if (route.path == '/blog') {
+        setScrollTop(blogScrollPos.value);
+      }
+    };
+
+    // onRouteChange
+    watch(() => route.path,
+      async () => {
+        await router.isReady();
+        if (route.path.includes('/blog')) {
+          setBlogScrollPos(); return;
+        }
+        setScrollTop(0);
+      }
+    );
+  }
+});
+</script>
