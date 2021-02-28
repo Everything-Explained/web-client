33728518994d4447865cfd0f151e2982113f8927
Jaeiya <exanix@gmail.com>
2021-02-27 16:39:29 -0800

    Add relative time utility

--- src/composeables/date-utils.ts
+++ src/composeables/date-utils.ts
@@ -4,7 +4,7 @@ type ISODateStr = string;
 
 
 
-const toDateObj = (date: ISODateStr|Date) => (
+const dateToObj = (date: ISODateStr|Date) => (
   typeof date == 'string' ? new Date(date) : date
 );
 
@@ -21,7 +21,7 @@ const getAMPM = (hours: number) => (
 );
 
 const getDateStrings = (date: ISODateStr|Date) => {
-  const dateObj = toDateObj(date);
+  const dateObj = dateToObj(date);
   return {
     month: padTime(dateObj.getMonth() + 1), // 0 is first month
     day: padTime(dateObj.getDate()),
@@ -30,7 +30,7 @@ const getDateStrings = (date: ISODateStr|Date) => {
 };
 
 const getTimeStrings = (date: ISODateStr|Date) => {
-  const dateObj = toDateObj(date);
+  const dateObj = dateToObj(date);
   return {
     hours   : `${dateObj.getHours()}`,
     minutes : padTime(dateObj.getMinutes()),
@@ -40,7 +40,7 @@ const getTimeStrings = (date: ISODateStr|Date) => {
 };
 
 const get12HourTimeStrings = (date: ISODateStr|Date) => {
-  const dateObj = toDateObj(date);
+  const dateObj = dateToObj(date);
   const ts = getTimeStrings(dateObj);
   return {
     ...ts,
@@ -71,6 +71,32 @@ export function dateToShortYMD(date: ISODateStr|Date) {
 export function dateToShortDMY(date: ISODateStr|Date) {
   const ds = getDateStrings(date);
   return `${ds.day}-${ds.month}-${ds.year}`;
+}
+
+export function dateToRelativeTimeStr(date: ISODateStr|Date) {
+  if (!Intl || !Intl.RelativeTimeFormat)
+    return dateToShortDMY(date) // Support old iOS version
+  ;
+  const rtf  = new Intl.RelativeTimeFormat('en', { numeric: 'auto' });
+  const dObj = dateToObj(date);
+  const diff = Math.floor((Date.now() - dObj.getTime()) / 1000);
+
+  const timeSpan = {
+    year   : 31536000,
+    month  : 2592000,
+    day    : 86400,
+    hour   : 3600,
+    minute : 60,
+    second : 1,
+  };
+
+  let span: keyof typeof timeSpan;
+  for (span in timeSpan) {
+    if (diff <= 3) return "a moment ago";
+    if (diff > timeSpan[span]) {
+      return rtf.format(-Math.floor(diff / timeSpan[span]), span);
+    }
+  }
 }
 
 
