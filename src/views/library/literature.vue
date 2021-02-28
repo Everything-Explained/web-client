7174e80f5d3025009e370e98905fc4803c4946e2
Jaeiya <exanix@gmail.com>
2021-02-27 19:06:07 -0800

    Add WIP literature component
    * upd(css): add subtle scrollbar style
    
    ref #42

--- src/views/library/literature.vue
+++ src/views/library/literature.vue
@@ -1,26 +1,58 @@
 <template>
-  <div class="temp">
+  <div class="lib-lit">
     <ee-titlebar :ease-in="350" :ease-out="350">
       Library Literature
     </ee-titlebar>
+    <div class="lib-lit__cards">
+      <div v-for="(article, i) of articles"
+           :key="i"
+           class="card"
+      >
+        <header>{{ article.title }}</header>
+        <article class="--subtle-scrollbar">
+          {{ article.summary }}
+        </article>
+        <footer>
+          by {{ article.author }}
+          &#x2022;
+          {{ useDate(article.date).toRelativeTime() }}
+        </footer>
+      </div>
+    </div>
+    <!-- <ee-footer /> -->
   </div>
 </template>
 
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import { useStore } from "vuex";
 import { VuexStore } from "@/vuex/vuex-store";
 import eeTitlebarVue from "@/components/layout/ee-titlebar.vue";
+import { useTask } from "vue-concurrency";
+import { useDataAPI } from "@/services/api_internal";
+import eeFooterVue from "@/components/layout/ee-footer.vue";
+import { useDate } from "@/composeables/date";
 
 export default defineComponent({
   components: {
     'ee-titlebar': eeTitlebarVue,
+    'ee-footer': eeFooterVue,
   },
   setup() {
     const store = useStore<VuexStore>();
+    const api = useDataAPI();
+    const articles = computed(() => store.state.dataCache['red33m/literature']);
 
-    store.commit('page-title', 'Literature');
+    const getLiterature = useTask(function*() {
+      const data = yield api.get('/red33m/literature', console.error);
+      store.commit('data-cache-add', { name: 'red33m/literature', data });
+    });
+
+
+    getLiterature.perform();
+
+    return { articles, useDate };
   }
 });
 </script>
