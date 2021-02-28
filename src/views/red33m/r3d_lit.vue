90d5acc7ffa0bcd6f2db3ccaa66088961138844d
Jaeiya <exanix@gmail.com>
2021-02-28 13:30:46 -0800

    Add literature to both library and RED33M categories
    ref #42

---
+++ src/views/red33m/r3d_lit.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="r3d-lit">
+    <ee-literature title="RED33M Literature"
+                   url="/red33m/literature"
+    />
+    <ee-footer />
+  </div>
+</template>
+
+
+<script lang="ts">
+import { defineComponent } from "vue"
+;
+import eeLiteratureVue from "@/components/layout/ee-literature.vue";
+import eeFooterVue from "@/components/layout/ee-footer.vue";
+
+
+export default defineComponent({
+  components: {
+    'ee-footer': eeFooterVue,
+    'ee-literature': eeLiteratureVue,
+  }
+});
+</script>
