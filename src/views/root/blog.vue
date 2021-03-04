1b4e1c0dc25a6302f5a635f151eed115eae54242
Jaeiya <exanix@gmail.com>
2021-03-04 15:37:35 -0800

    fix(blog): deprecated code causing multiple requests

--- src/views/root/blog.vue
+++ src/views/root/blog.vue
@@ -11,27 +11,13 @@
 
 
 <script lang='ts'>
-import { computed, defineComponent } from "vue";
+import { defineComponent } from "vue"
+;
 import eeFooterVue   from '@/components/layout/ee-footer.vue';
-import { StaticPage, useStaticPager } from "@/composeables/staticPager";
 import eeLiteratureVue from "@/components/layout/ee-literature.vue";
-
-
-
-interface BlogPost extends StaticPage {
-  image_header?: string;
-  summary: string;
-}
 
 
 export default defineComponent({
   components: { 'ee-footer': eeFooterVue, 'ee-literature': eeLiteratureVue, },
-  setup() {
-    const pager    = useStaticPager<BlogPost>('/blog');
-    const titleRef = computed(
-      () => pager.pageTitle.value || 'Blog Entries'
-    );
-    return { ...pager, titleRef, };
-  },
 });
 </script>
