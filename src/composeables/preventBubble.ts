2929ae817e680043a7e0245cb65c0d6f0ddd5f81
Jaeiya <exanix@gmail.com>
2021-03-05 18:44:47 -0800

    Delete usePreventBubble()
    This is actually just a duplication of "@event.prevent"
    which is built into Vue.

--- src/composeables/preventBubble.ts
+++
@@ -1,8 +0,0 @@
-
-
-export function usePreventBubble() {
-  return function preventBubble(e: MouseEvent|KeyboardEvent|WheelEvent) {
-    e.preventDefault();
-    e.stopImmediatePropagation();
-  };
-}
