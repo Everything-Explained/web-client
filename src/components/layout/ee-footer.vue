f71dcb91b10f6f6cee7ca651c9f0d3d75c7d5e1f
Jaeiya <exanix@gmail.com>
2021-05-20 14:41:45 -0700

    upd(footer): add version and named link
    ref #52

--- src/components/layout/ee-footer.vue
+++ src/components/layout/ee-footer.vue
@@ -1,16 +1,20 @@
 <template>
   <footer ref="footRef" class="app-footer">
+    <span class="app-footer__version">
+      α<sup>2<sub><router-link :to="'/changelog/a2-snickerdoodle'">snickerdoodle</router-link></sub></sup>
+    </span>
     <span class="app-footer_copyright">
       <span>Copyright &#169; 2020 - 2021</span><br>
+      <span>Everything Explained, Some Rights Reserved</span> <br>
       <a
         rel="license"
         href="https://creativecommons.org/licenses/by-nc-sa/4.0/"
         target="_blank"
       >Creative Commons BY-NC-SA-4.0 License</a><br>
-      <span>Everything Explained, Some Rights Reserved</span> <br>
     </span>
   </footer>
 </template>
+
 
 <script lang="ts">
 import { defineComponent, onMounted, onUnmounted, ref } from "vue";
