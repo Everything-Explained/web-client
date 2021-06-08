a3473f72f13bda0e6d3ffea1a36c040cddce32b2
Jaeiya <exanix@gmail.com>
2021-06-07 18:32:42 -0700

    Add WIP checkbox component

---
+++ src/components/ui/ee-checkbox.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="ee-chkbx__container">
+    <input
+      :id="chkbxID"
+      class="ee-chkbx"
+      type="checkbox"
+    >
+    <label :for="chkbxID" class="ee-chkbx__label">
+      <div>
+        <svg class="ee-chkbx__svg-ref">
+          <use :xlink:href="'#' + svgID" />
+        </svg>
+      </div>
+      <div>Testing 123</div>
+    </label>
+    <svg class="ee-chkbx__checkmark">
+      <symbol :id="svgID" viewbox="0 0 12 10">
+        <polyline points="1.5 6 4.5 9 10.5 1" />
+      </symbol>
+    </svg>
+  </div>
+</template>
+
+
+
+<script lang="ts">
+import useUniqueIDGen from "@/composeables/useUniqueID";
+import { defineComponent } from "@vue/runtime-core";
+
+
+export default defineComponent({
+  setup() {
+    const genID = useUniqueIDGen().genID;
+
+    return {
+      svgID  : genID(),
+      chkbxID: genID(),
+    };
+  }
+});
+
+</script>
+
+
+
+
