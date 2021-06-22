39afa2230a965ce8c396c0b3961049160f776d47
Jaeiya <exanix@gmail.com>
2021-06-21 19:54:05 -0700

    upd(lib-videos): add WIP category cards

--- src/views/library/lib.videos.vue
+++ src/views/library/lib.videos.vue
@@ -1,12 +1,38 @@
 <template>
-  <div class="lib-videos">
+  <div class="lib-vid">
     <ee-titlebar :ease-in="350" :ease-out="350">
       Library Videos
     </ee-titlebar>
     <transition name="fade" mode="out-in">
       <div v-if="isVideoTaskRunning" class="preloader page" />
       <div v-else>
-        <div
+        <div class="lib-vid__categories">
+          <div v-for="(cat, i) of categories"
+               :key="i"
+               class="lib-vid-category__container"
+          >
+            <div class="lib-vid__category">
+              <h1>{{ cat.name }}</h1>
+              <div class="lib-vid-category__desc">
+                Cillum adipisicing do sint velit nisi proident. Mollit minim anim reprehenderit sunt
+                ea incididunt elit ipsum sint dolor. Officia esse id occaecat.
+              </div>
+              <h2>Contributing Authors</h2>
+              <div class="lib-vid-category__desc --authors">
+                <ul>
+                  <li v-for="(author, j) of getAuthors(cat.videos)"
+                      :key="j"
+                      :class="{ '--is-ethan': isEthan(author) }"
+                  >
+                    {{ author }}
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <footer>Updated {{ useDate(cat.videos[cat.videos.length - 1].date).toRelativeTime() }}</footer>
+          </div>
+        </div>
+        <!-- <div
           v-for="(cat, i) of videoCategories"
           :key="i"
           class="category"
@@ -22,11 +48,12 @@
               :video-id="v.id"
               :desc="v.content"
               :date="v.date"
+              :author="v.author"
             >
               {{ v.title }}
             </ee-video>
           </div>
-        </div>
+        </div> -->
         <ee-footer />
       </div>
     </transition>
@@ -42,6 +69,8 @@ import eeVideo       from "@/components/ui/ee-video.vue";
 import eeTitlebarVue from "@/components/layout/ee-titlebar.vue";
 import useVideos from "@/composeables/useVideos";
 import { Video } from "@/typings/global-types";
+import { useDate } from "@/composeables/date";
+import { isEthan } from "@/composeables/globals";
 
 type VideoCategories = { name: string; videos: Video[] };
 
@@ -52,12 +81,22 @@ export default defineComponent({
     'ee-footer'   : eeFooterVue,
   },
   setup() {
-    const { videos: videoCategories, getVideoTask } = useVideos<VideoCategories>('/data/library/videos.json');
-
+    const { videos: categories, getVideoTask } = useVideos<VideoCategories>('/data/library/videos.json');
     const videoTask = getVideoTask(() => void(0));
     videoTask.loadVideos();
 
-    return { videoCategories, isVideoTaskRunning: videoTask.isRunning };
+    const getAuthors = (videos: Video[]) => {
+      return [
+        'Ethan Kahn', 'Vojtěch Kantor', 'KeSyia', 'Bilbou Baggins', 'Piccolo', 'Pippa Pig', 'Fievel Moskowitz'
+      ]
+      // return videos.reduce((authors, video) => {
+      //   if (authors.includes(video.author)) return authors;
+      //   authors.push(video.author);
+      //   return authors;
+      // }, [] as string[]);
+    };
+
+    return { categories, isVideoTaskRunning: videoTask.isRunning, useDate, getAuthors, isEthan };
   }
 });
 </script>
