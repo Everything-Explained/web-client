3e4ba391a299cd199928e7b38d1789b4e3be30ef
Jaeiya <exanix@gmail.com>
2021-06-24 15:43:46 -0700

    upd(router): add URI param (forgot to commit this)

--- src/router/index.ts
+++ src/router/index.ts
@@ -24,7 +24,7 @@ const routes: Array<RouteRecordRaw> = [
   { path: '/red33m/literature/:page?',  name: 'r3d-lit',     component: r3d_litVue, beforeEnter: isAuthedGuard },
   { path: '/red33m/auth',               name: 'red33m-auth', component: red33mAuth,   },
   { path: '/red33m/form',               name: 'red33m-form', component: red33mForm,   },
-  { path: '/library/videos',            name: 'lib-videos',  component: Videos,       },
+  { path: '/library/videos/:uri?',      name: 'lib-videos',  component: Videos,       },
   { path: '/library/literature/:page?', name: 'lib-lit',     component: Literature,   },
   { path: '/changelog/:page?',          name: 'changelog',   component: changelogVue, },
   { path: '/:pathMatch(.*)*',           name: '404',         component: i404,       meta: { display: false } }
