360281c9b56e1c4178b556a790cf5a05653ea2e3
Jaeiya <exanix@gmail.com>
2021-06-25 15:38:54 -0700

    fix(dyn-pager): active page was not updating
    Turns out the computed value doesn't work how I was hoping
    and I needed a way to set the active page manually anyway
    with certain route updates.

--- src/composeables/dynamicPager.ts
+++ src/composeables/dynamicPager.ts
@@ -1,51 +1,63 @@
-import { computed, watch } from "vue";
+import { computed, ref, watch } from "vue";
 import { useRouter } from "vue-router";
 import { useURI } from "./useURI";
 
-type URIMap = {
-  [key: string]: {
-    title: string;
-    uri: string;
-    data: any[];
-  }
+
+type DynamicPage = {
+  title: string;
+  uri  : string;
+  data : any[];
 }
+
+type URIMap = { [key: string]: DynamicPage; }
+
+
 
 export function useDynamicPager(url: string) {
   const router      = useRouter();
   const route       = router.currentRoute;
-  const pageMap      = {} as URIMap;
+  const currentURI  = computed(() => route.value.params.uri);
+  const pageMap     = {} as URIMap;
+  const activePage  = ref<DynamicPage>();
+
+  function setActivePage() {
+    activePage.value = Object.values(pageMap).find(p => p.uri == currentURI.value);
+  }
 
   // onRouteChange
   watch(() => route.value.params,
     (params) => {
       if (!route.value.path.includes(url)) return;
-      if (!params.uri) return;
+      if (!params.uri) { activePage.value = undefined; return; }
+      if (currentURI.value) setActivePage();
     }
   );
 
   return {
     setDynPages: (pages: { name: string; data: any[]; }[]) => {
-      pages.reduce((map, page) => {
-        map[page.name] = {
+      pages.forEach((page) => {
+        pageMap[page.name] = {
           title: page.name,
           uri: useURI(page.name),
           data: page.data
         };
-        return map;
-      }, pageMap);
+      });
+      // If route is already pointing to page
+      if (currentURI.value) setActivePage();
     },
 
     goTo: (pageName: string) => {
-      if (!pageMap[pageName]) {
+      const currentPage = pageMap[pageName];
+      if (!currentPage) {
         router.push('/404');
         throw Error(`dynamicPager()::Missing or Invalid Page Name "${pageName}"`);
       }
       router.push(`/${url}/${pageMap[pageName].uri}`);
+      activePage.value = currentPage;
     },
 
-    activePage: computed(
-      () => Object.values(pageMap).find(page => page.uri == route.value.params.uri)
-    ),
+    activePage,
+
   };
 }
 
